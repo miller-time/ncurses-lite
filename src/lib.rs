5216d425e048041d26ba383f4d8d077ca155f8c4
Russell <miller.time.baby@gmail.com>
2022-07-23 16:06:51 -0700

    add extern function definitions

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #![allow(non_camel_case_types)]
 
-use libc::c_int;
+use std::ffi::CString;
 
-pub type chtype = u32;
+use libc::{c_char, c_int};
 
 pub enum CURSOR_VISIBILITY {
     CURSOR_INVISIBLE,
@@ -10,24 +10,46 @@ pub enum CURSOR_VISIBILITY {
 
 pub type WINDOW = *mut i8;
 
-pub fn initscr() {}
+pub fn initscr() -> WINDOW {
+    unsafe { c_initscr() }
+}
 
-pub fn endwin() {}
+pub fn endwin() -> i32 {
+    unsafe { c_endwin() }
+}
 
-pub fn curs_set(_visibility: CURSOR_VISIBILITY) {}
+pub fn curs_set(visibility: CURSOR_VISIBILITY) -> i32 {
+    unsafe { c_curs_set(visibility as i32) }
+}
 
 pub fn newwin(lines: i32, cols: i32, y: i32, x: i32) -> WINDOW {
     unsafe { c_newwin(lines, cols, y, x) }
 }
 
-pub fn waddch(_w: WINDOW, _ch: chtype) {}
+pub fn waddch(w: WINDOW, ch: u32) -> i32 {
+    unsafe { c_waddch(w, ch) }
+}
 
-pub fn waddstr(_w: WINDOW, _s: &str) {}
+pub fn waddstr(w: WINDOW, s: &str) -> i32 {
+    let c_string = CString::new(s).unwrap();
+    unsafe { c_waddstr(w, c_string.as_ptr()) }
+}
 
-pub fn wclear(_w: WINDOW) {}
+pub fn wclear(w: WINDOW) -> i32 {
+    unsafe { c_wclear(w) }
+}
 
-pub fn wrefresh(_w: WINDOW) {}
+pub fn wrefresh(w: WINDOW) -> i32 {
+    unsafe { c_wrefresh(w) }
+}
 
 extern "C" {
+    fn c_initscr() -> WINDOW;
+    fn c_endwin() -> c_int;
+    fn c_curs_set(_: c_int) -> c_int;
     fn c_newwin(_: c_int, _: c_int, _: c_int, _: c_int) -> WINDOW;
+    fn c_waddch(_: WINDOW, _: u32) -> c_int;
+    fn c_waddstr(_: WINDOW, _: *const c_char) -> c_int;
+    fn c_wclear(_: WINDOW) -> c_int;
+    fn c_wrefresh(_: WINDOW) -> c_int;
 }
